7494dbcc838177c2cbbd7d3ac717b9c24982cb65
fahadfahim13 <fahadfahim13@gmail.com>
2024-10-14 03:04:30 +0600

    Removed access key, secret key

--- variables.tf
+++ variables.tf
@@ -1,12 +1,4 @@
-variable "aws_access_key" {
-  description = "The AWS access key"
-  type        = string
-}
 
-variable "aws_secret_key" {
-  description = "The AWS secret key"
-  type        = string
-}
 
 variable "aws_region" {
   description = "The AWS region to deploy the resources"
